332750f5973cb71650a05ad9118b2898b7bb89ea
Abhijith <abhijithrajeevan123@gmail.com>
2023-07-06 17:42:05 +0530

    completed the netflix using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {originals,action,romantic, trending} from './urls'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <NavBar/>
+        <Banner/>
+        <RowPost url={trending} title='Trending' />
+        <RowPost url={action} title='Action Movies' isSmall />
+        <RowPost url={romantic} title='Romantic Movies' isSmall />
+        <RowPost url={originals} title='NEtflix originals' isSmall />
+
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3/'
+export const API_KEY = 'aa6e42abad3478b1272596a3611d9562'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
